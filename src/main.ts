d8cefe841e70ba9e89d6f1b83e21e6fa303fa502
Ismael.Elias <ismael.eliass22@gmail.com>
2024-01-17 22:13:56 -0300

    FEAT: Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,13 @@ import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './exception/database.exception';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const { httpAdapter } = app.get(HttpAdapterHost);
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
   app.enableCors({
-    origin: true,
-    allowedHeaders: [
-      'XMLHttpRequest',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Observe',
-      'Authorization',
-      'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Origin',
-      'Access-Control-Request-Method',
-      'Access-Control-Request-Headers',
-    ],
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
+    origin: 'https://www.api.animaverso.com.br/',
   });
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   await app.listen(3000);
