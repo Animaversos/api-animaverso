b2a4121fb0cb43aba15f0b10cf7216ebf5e75813
Ismael.Elias <ismael.eliass22@gmail.com>
2024-01-17 23:12:26 -0300

    FEAT: Enable cors

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,6 @@ async function bootstrap() {
       if (whitelist.indexOf(origin) !== -1) {
         console.log('allowed cors for:', origin);
         callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
       }
     },
     allowedHeaders:
