e4e6867723c3683fa338a15aec3115944f203113
Ismael.Elias <ismael.eliass22@gmail.com>
2024-01-17 23:29:39 -0300

    FEAT: disable cors

--- src/main.ts
+++ src/main.ts
@@ -9,24 +9,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
-  const whitelist = ['https://animaverso.com.br', 'http://animaverso.com.br'];
-  app.enableCors({
-    origin: function (origin, callback) {
-      let corsOptions;
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        corsOptions = { origin: true };
-      } else {
-        console.log('blocked cors for:', origin);
-        corsOptions = { origin: false };
-      }
-      callback(null, corsOptions);
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
+
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   await app.listen(3000);
 }
